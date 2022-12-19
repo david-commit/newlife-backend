9e0bf17ce17f79cb2f42f0eae1478a4ff41e596e
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 18:52:47 +0300

    Clear payment table before modifying it for tests

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -2,17 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
   it "can be created" do
+    Payment.destroy_all
+
     payment = Payment.create!(payment_method: "mpesa")
     expect(payment.id.nil?).to eql(false)
   end
 
   it "can be updated" do
-     payment = Payment.create!(payment_method: "mpesa")
-     payment.update(payment_method: "paypal")
-     expect(payment.payment_method).to eql("paypal")
+    Payment.destroy_all
+
+    payment = Payment.create!(payment_method: "mpesa")
+    payment.update(payment_method: "paypal")
+    expect(payment.payment_method).to eql("paypal")
   end
 
   it "can be deleted" do
+    Payment.destroy_all
+    
     payment = Payment.create!(payment_method: "mpesa")
     payment_id = payment.id
     payment.destroy
