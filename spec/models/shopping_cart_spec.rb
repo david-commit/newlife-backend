127bf7729baf96e2711a903a02bd4d025f8ca7f1
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 19:24:59 +0300

    Add tests to shoping cart

--- spec/models/shopping_cart_spec.rb
+++ spec/models/shopping_cart_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe ShoppingCart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created using valid data" do
+
+    User.destroy_all
+    user = User.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Payment.destroy_all
+    payment = Payment.create!(payment_method: "mpesa")
+    expect(payment.id.nil?).to eql(false)
+
+    Order.destroy_all
+    order = Order.create!(
+      user_id: user.id,
+      payment_id: payment.id,
+      delivered: false
+    )
+
+    Product.destroy_all
+    product = Product.create!(
+      name: "What",
+      category: "Whatever",
+      price: 2.67
+    )
+
+    ShoppingCart.destroy_all
+    shopping_cart = ShoppingCart.create!(
+      order_id: order.id,
+      product_id: product.id
+    )
+
+    expect(shopping_cart.id.nil?).to eql(false)
+  end
+
+  it "cannot be created with invalid data" do
+    expect{ShoppingCart.create!()}.to raise_error ActiveRecord::RecordInvalid
+  end
 end
