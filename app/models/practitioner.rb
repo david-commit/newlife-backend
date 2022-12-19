f18ed1ee89cc5b5db007e56a03a13be3dba9833c
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 20:48:52 +0300

    Add Practitioner && Department relationships

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -3,6 +3,8 @@ class Practitioner < ApplicationRecord
 
     has_many :appointments, dependent: :destroy
     has_one :practitioner_profile, dependent: :destroy
+    has_many :department_practitioners
+    has_many :departments, through: :department_practitioners
 
     validates :username, presence: true
     validates :password, presence: true
--- spec/models/department_practitioner_spec.rb
+++ spec/models/department_practitioner_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe DepartmentPractitioner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created with valid data" do
+    Department.destroy_all
+    department = Department.create(name: "Human Resource")
+
+    Practitioner.destroy_all
+    practitioner = Practitioner.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    DepartmentPractitioner.destroy_all
+    department_practitioner = DepartmentPractitioner.create(
+      department_id: department.id,
+      practitioner_id: practitioner.id
+    )
+  end
+
+  it "cannot be created with invalid data" do
+    expect{DepartmentPractitioner.create!()}.to raise_error ActiveRecord::RecordInvalid
+  end
 end
