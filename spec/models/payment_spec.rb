ebdcd9a557e67118b67e2c1651feb8d81ca0b4c8
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 18:08:50 +0300

    Add tests to the payment model

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created" do
+    payment = Payment.create!(payment_method: "mpesa")
+    expect(payment.id.nil?).to eql(false)
+  end
+
+  it "can be updated" do
+     payment = Payment.create!(payment_method: "mpesa")
+     payment.update(payment_method: "paypal")
+     expect(payment.payment_method).to eql("paypal")
+  end
+
+  it "can be deleted" do
+    payment = Payment.create!(payment_method: "mpesa")
+    payment_id = payment.id
+    payment.destroy
+    expect{Payment.find(payment_id)}.to raise_error ActiveRecord::RecordNotFound
+  end
 end
