11885fc5956ca155930246b07521cc916441bb85
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 20:17:39 +0300

    Add tests to Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :appointment
+
+  validates :appointment_id, presence: true
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "can be created using valid inputs" do
+    User.destroy_all
+    user = User.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Practitioner.destroy_all
+    practitioner = Practitioner.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Appointment.destroy_all
+    appointment = Appointment.create!(
+      user_id: user.id,
+      practitioner_id: practitioner.id
+    )
+
+    Message.destroy_all
+    message = Message.create(
+      appointment_id: appointment.id
+    )
+
+    expect(message.id.nil?).to eql(false)
+  end
+
+  it "is deleted when the appointment it belongs to is deleted" do 
+    User.destroy_all
+    user = User.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Practitioner.destroy_all
+    practitioner = Practitioner.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Appointment.destroy_all
+    appointment = Appointment.create!(
+      user_id: user.id,
+      practitioner_id: practitioner.id
+    )
+
+    Message.destroy_all
+    message = Message.create(
+      appointment_id: appointment.id
+    )
+
+    message_id = message.id
+    appointment.destroy
+
+    expect{Message.find(message_id)}.to raise_error ActiveRecord::RecordNotFound
+  end        
 end
