f8c7abfb5fb722cdb0eb801fdba19a0d2f11f8ba
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 18:57:38 +0300

    Add "delivered" column to order table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_150021) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_155656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_150021) do
     t.bigint "payment_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "delivered"
     t.index ["payment_id"], name: "index_orders_on_payment_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
