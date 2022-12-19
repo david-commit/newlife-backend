cfed7b8f5b3269799cbcb591b991a1acb711e416
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 18:51:15 +0300

    Add test to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :appointments
+    has_one :patient_profile
+    
+    validates :username, presence: true
+    validates :password, presence: true
+    validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created with valid inputs (must contain username, email, and password)" do
+    User.destroy_all
+
+    user = User.create!(
+      username: Faker::Internet.username(specifier: 5..15),
+      password: Faker::Internet.username,
+      email: Faker::Internet.email
+    )
+
+    expect(user.id.nil?).to eql(false)
+    
+  end
+
+  it "cannot be created with invalid inputs (one of username, email, or password is missing)" do
+    User.destroy_all
+
+    expect{
+        User.create!(
+        username: "",
+        password: "",
+        email: ""
+      )
+    }.to raise_error ActiveRecord::RecordInvalid  
+  end
+
+  it "can have appointments" do
+    User.destroy_all
+
+    user = User.create!(
+      username: Faker::Internet.username(specifier: 5..15),
+      password: Faker::Internet.username,
+      email: Faker::Internet.email
+    )
+
+    expect(user.appointments).to be_kind_of(ActiveRecord::Associations::CollectionProxy)
+  end
 end
