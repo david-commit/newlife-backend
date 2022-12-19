f320386c60735b6af9b327f8dea84043b8c5e6f6
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 19:07:57 +0300

    Add teset to the Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created with valid data (must contain user and payment)" do
+
+    User.destroy_all
+    user = User.create!(
+      username: Faker::Internet.username(specifier: 5..15),
+      password: Faker::Internet.username,
+      email: Faker::Internet.email
+    )
+
+    Payment.destroy_all
+    payment = Payment.create!(payment_method: "mpesa")
+
+    Order.destroy_all
+    order = Order.create!(
+      user_id: user.id,
+      payment_id: payment.id,
+      delivered: false
+    )
+
+    expect(order.id.nil?).to eql(false)
+  end
+
+  it "cannot be created with invalid data" do
+    Order.destroy_all
+    expect{Order.create!()}.to raise_error ActiveRecord::RecordInvalid 
+  end
 end
