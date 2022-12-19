0b17407c85188aacbc453d38000e5f8430131ebc
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 20:09:37 +0300

    Add tests to the Appointment model

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,105 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created using valid inputs" do
+    User.destroy_all
+    user = User.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Practitioner.destroy_all
+    practitioner = Practitioner.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Appointment.destroy_all
+    appointment = Appointment.create!(
+      user_id: user.id,
+      practitioner_id: practitioner.id
+    )
+
+    expect(appointment.id.nil?).to eql(false)
+  end
+
+  it "can have many messages" do
+    User.destroy_all
+    user = User.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Practitioner.destroy_all
+    practitioner = Practitioner.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Appointment.destroy_all
+    appointment = Appointment.create!(
+      user_id: user.id,
+      practitioner_id: practitioner.id
+    )
+
+    expect(appointment.messages).to be_kind_of(ActiveRecord::Associations::CollectionProxy)
+  end
+
+  it "is deleted when a the patient it belongs to is deleted" do
+     User.destroy_all
+    user = User.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Practitioner.destroy_all
+    practitioner = Practitioner.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Appointment.destroy_all
+    appointment = Appointment.create!(
+      user_id: user.id,
+      practitioner_id: practitioner.id
+    )  
+    
+    appointment_id = appointment.id
+    user.destroy
+
+    expect{Appointment.find(appointment_id)}.to raise_error ActiveRecord::RecordNotFound
+  end
+
+  it "is deleted when a the practitioner it belongs to is deleted" do
+    User.destroy_all
+    user = User.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Practitioner.destroy_all
+    practitioner = Practitioner.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Appointment.destroy_all
+    appointment = Appointment.create!(
+      user_id: user.id,
+      practitioner_id: practitioner.id
+    )  
+    
+    appointment_id = appointment.id
+    practitioner.destroy
+
+    expect{Appointment.find(appointment_id)}.to raise_error ActiveRecord::RecordNotFound
+  end
 end
