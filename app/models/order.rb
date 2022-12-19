3c8eb8fc7fbc6303ee1d07e0117cbc5f544d5ac3
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 20:22:26 +0300

    Destroy a shopping cart when an order is destroyed
    Since the ShoppingCart model belongs to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :payment
-  has_many :shopping_carts
+  has_many :shopping_carts, dependent: :destroy
 
   validates :user, presence: true
   validates :payment, presence: true
--- spec/models/shopping_cart_spec.rb
+++ spec/models/shopping_cart_spec.rb
@@ -40,4 +40,42 @@ RSpec.describe ShoppingCart, type: :model do
   it "cannot be created with invalid data" do
     expect{ShoppingCart.create!()}.to raise_error ActiveRecord::RecordInvalid
   end
+
+  it "is deleted when an order is deleted" do
+    User.destroy_all
+    user = User.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    Payment.destroy_all
+    payment = Payment.create!(payment_method: "mpesa")
+    expect(payment.id.nil?).to eql(false)
+
+    Order.destroy_all
+    order = Order.create!(
+      user_id: user.id,
+      payment_id: payment.id,
+      delivered: false
+    )
+
+    Product.destroy_all
+    product = Product.create!(
+      name: "What",
+      category: "Whatever",
+      price: 2.67
+    )
+
+    ShoppingCart.destroy_all
+    shopping_cart = ShoppingCart.create!(
+      order_id: order.id,
+      product_id: product.id
+    )
+    
+    shopping_cart_id = shopping_cart.id
+    order.destroy
+
+    expect{ShoppingCart.find(shopping_cart_id)}.to raise_error ActiveRecord::RecordNotFound
+  end
 end
