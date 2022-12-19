811e0fff9b98732b002672ddffa08605f69a6465
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 19:14:25 +0300

    Add test to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    validates :name, presence: true
+    validates :category, presence: true
+    validates :price, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created with valid data" do
+    Product.destroy_all
+
+    product = Product.create!(
+      name: "What",
+      category: "Whatever",
+      price: 2.67
+    )
+
+    expect(product.id.nil?).to eql(false)
+  end
+
+  it "cannot be created with invalid data" do
+    expect{Product.create!()}.to raise_error ActiveRecord::RecordInvalid
+  end
 end
