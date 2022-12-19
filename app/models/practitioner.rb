bfd893e6a59330178e54d4471608b9089c918734
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-19 19:31:12 +0300

    Add test to Practitioner model

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -1,2 +1,9 @@
 class Practitioner < ApplicationRecord
+    has_secure_password
+
+    has_many :appointments
+
+    validates :username, presence: true
+    validates :password, presence: true
+    validates :email, presence: true
 end
--- spec/models/practitioner_spec.rb
+++ spec/models/practitioner_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Practitioner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created with valid inputs (must contain username, email, and password)" do
+    Practitioner.destroy_all
+    user = Practitioner.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    expect(user.id.nil?).to eql(false)
+  end
+
+  it "cannot be created with invalid inputs (one of username, email, or password is missing)" do
+    Practitioner.destroy_all
+    expect{Practitioner.create!()}.to raise_error ActiveRecord::RecordInvalid  
+  end
+
+  it "can have appointments" do
+    Practitioner.destroy_all
+
+    user = Practitioner.create!(
+      username: "vincent",
+      password: "vincent",
+      email: "vincent@vincent.com"
+    )
+
+    expect(user.appointments).to be_kind_of(ActiveRecord::Associations::CollectionProxy)
+  end
 end
