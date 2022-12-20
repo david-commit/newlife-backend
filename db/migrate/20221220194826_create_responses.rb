bf3960b11dd292653743823418b70365bab36bd2
Enock <enock.mokua@student.moringaschool.com>
2022-12-20 23:08:07 +0300

    add response attributes

--- db/migrate/20221220194826_create_responses.rb
+++ db/migrate/20221220194826_create_responses.rb
@@ -1,6 +1,11 @@
 class CreateResponses < ActiveRecord::Migration[7.0]
   def change
     create_table :responses do |t|
+      t.belongs_to :payment, foreign_key: true
+      t.string :merchant_request_id
+      t.string :response_description
+      t.integer :response_code
+      t.string :customer_message
 
       t.timestamps
     end
