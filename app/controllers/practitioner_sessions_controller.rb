06dda37e097cdc621da2f9b1eb620e577677ee8d
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-20 19:32:10 +0300

    Enable a practitioner to log in
    Like users, we have not set up sessions here, but we will do some times later on. Also, we have added the spec file for testing

---
+++ app/controllers/practitioner_sessions_controller.rb
@@ -0,0 +1,22 @@
+class PractitionerSessionsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    def create
+        practitioner = Practitioner.find_by!(username: params[:username])
+        render json: practitioner.authenticate(params[:password]), status: :ok
+    end
+
+    def destroy
+        #session.delete :user_id
+        #head 202
+    end
+
+    private
+    def user_params
+        params.permit(:username, :password)
+    end
+
+    def record_not_found(not_found)
+        render json: not_found, status: 404
+    end      
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  post '/practitioner/login', to: 'practitioner_sessions#create'
+  delete '/practitioner/logout', to: 'practitioner_sessions#destroy'
+
   
   # resources :departments
   # resources :practitioner_profiles
