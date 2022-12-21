4d9853310a37c25f69bdd686dec1ecf56e2cdce5
Vincent Makokha <makokhavomondi@gmail.com>
2022-12-21 17:57:15 +0300

    Enable practitioner signup and test it

--- spec/requests/practitioners_spec.rb
+++ spec/requests/practitioners_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Practitioners", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  it "allows one to signup" do
+    Department.destroy_all
+    department = Department.create(name: "Human Resource")
+
+    Practitioner.find_by(username: "what")&.destroy_all
+    Practitioner.find_by(email: "email")&.destroy_all
+
+    headers = {"ContentType": "application/json"}
+    post '/practitioner/signup', params: {"username": "what", "password": "what", "email": "what@what.com", "department_id": department.id}
+
+    expect(response).to have_http_status(201)
   end
 end
