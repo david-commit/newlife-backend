a5ce396895be1ce4233e429ec4eba0a7f4743623
Enock <enock.mokua@student.moringaschool.com>
2023-01-04 17:11:51 +0300

    get all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,36 @@
 class UsersController < ApplicationController
-    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-    def create
-        user = User.create!(user_params)
-        # session[:user_id] = user.id
-        render json: user, status: :created
-    end
+  def index
+    render json: User.all
+  end
 
-    def show
-        # render json: User.find(session[:user_id]), status: :ok
-        render json: User.find(params[:id])
-    end
+  def create
+    user = User.create!(user_params)
+    # session[:user_id] = user.id
+    render json: user, status: :created
+  end
 
-    private
-    def user_params
-        params.permit(:password_confirmation, :password, :email, :username)
-    end
+  def show
+    # render json: User.find(session[:user_id]), status: :ok
+    render json: User.find(params[:id])
+  end
 
-    def record_invalid(invalid)
-        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
-    end
+  private
 
-    def record_not_found(not_found)
-        render json: not_found, status: 404
-    end
+  def user_params
+    params.permit(:password_confirmation, :password, :email, :username)
+  end
+
+  def record_invalid(invalid)
+    render json: {
+             errors: invalid.record.errors.full_messages
+           },
+           status: :unprocessable_entity
+  end
+
+  def record_not_found(not_found)
+    render json: not_found, status: 404
+  end
 end
