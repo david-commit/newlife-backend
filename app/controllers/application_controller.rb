60a7b37179b0197bb6dbffbe520f15a5a3c060b2
Enock <enock.mokua@student.moringaschool.com>
2023-01-04 19:53:41 +0300

    update current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,5 +24,9 @@ class ApplicationController < ActionController::API
   end
 
   def current_user
+    if decoded_token
+      user_id = decoded_token[0]["user_id"]
+      @user = User.find_by(id: user_id)
+    end
   end
 end
