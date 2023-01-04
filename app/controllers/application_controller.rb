bcde7a575c66504aa5392a78d7e0596a4410a040
Enock <enock.mokua@student.moringaschool.com>
2023-01-04 19:52:20 +0300

    update decode token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,23 @@ class ApplicationController < ActionController::API
     JWT.encode(payload, "my_s3cr3t")
   end
 
+  # authorization bearer token
   def auth_header
-  end
-
-  def auth_header
+    request.headers["Authorization"]
   end
 
   def decoded_token
+    if auth_token
+      token = auth_header.split(" ")[1]
+      begin
+        JWT.decode(token, "my_s3cr3t", true, algorithm: "HS256")
+      rescue JWT::DecodeError
+        nil
+      else
+      end
+    end
+  end
+
+  def current_user
   end
 end
