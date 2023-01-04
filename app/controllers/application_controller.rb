810881ba2197fc88d26d03d5cd0821ececbccda8
Enock <enock.mokua@student.moringaschool.com>
2023-01-04 19:48:11 +0300

    update encode_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
   before_action :authorized
-  
-  def encode_token
+
+  def encode_token(payload)
+    #store secret in env variable
+    JWT.encode(payload, "my_s3cr3t")
+  end
+
+  def auth_header
   end
 
   def auth_header
