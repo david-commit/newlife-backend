96f898959eb3b3f930702cf8b46b0e0a950d7071
Enock <enock.mokua@student.moringaschool.com>
2023-01-05 11:16:48 +0300

    add authenticate create action

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,6 +3,20 @@ class AuthenticationController < ApplicationController
 
   def create
     @user = User.find_by(username: user_login_params[:username])
+
+    if @user && @user.authenticate(user_login_params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: {
+               user: UserSerializer.new(@user),
+               jwt: token
+             },
+             status: :accepted
+    else
+      render json: {
+               message: "Invalid username or password"
+             },
+             status: :unauthorized
+    end
   end
 
   private
