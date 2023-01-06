9efcb5811a1133be4a7fa03b72ac32c6f91c81f2
Enock <enock.mokua@student.moringaschool.com>
2023-01-07 02:20:33 +0300

    update create sessions

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,12 +2,12 @@ class AuthenticationController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    @user = User.find_by(username: user_login_params[:email])
+    user = User.find_by_email(user_login_params[:email])
 
-    if @user && @user.authenticate(user_login_params[:password])
-      token = encode_token({ user_id: @user.id })
+    if user && user.authenticate(user_login_params[:password])
+      token = issue_token(user)
       render json: {
-               user: UserSerializer.new(@user),
+               user: UserSerializer.new(user),
                jwt: token
              },
              status: :accepted
