2ead047d9f0f47a4a1d76544d0dd5f033c6b5731
Enock <enock.mokua@student.moringaschool.com>
2023-01-07 01:28:02 +0300

    create show action for users

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,7 +2,7 @@ class AuthenticationController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    @user = User.find_by(username: user_login_params[:username])
+    @user = User.find_by(username: user_login_params[:email])
 
     if @user && @user.authenticate(user_login_params[:password])
       token = encode_token({ user_id: @user.id })
@@ -19,9 +19,13 @@ class AuthenticationController < ApplicationController
     end
   end
 
+  def show
+    
+  end
+
   private
 
   def user_login_params
-    params.require(:user).permit(:email, :password)
+    params.permit(:email, :password)
   end
 end
