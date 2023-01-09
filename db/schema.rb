783beae17d8695bd19eaefaea6bd099712d65107
Vincent Makokha <makokhavomondi@gmail.com>
2023-01-09 17:47:23 +0300

    Add image and dosage to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_115049) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_144602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -119,6 +119,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_115049) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.string "image"
+    t.string "dosage"
   end
 
   create_table "responses", force: :cascade do |t|
