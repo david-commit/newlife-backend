c3e906bd5d1f6e5bcbb4ef358ab167787a8768fc
Vincent Makokha <makokhavomondi@gmail.com>
2023-01-09 18:40:24 +0300

    Sanitize development branch
    Basically make sure stuff are consistent. Also ensure the failing tests are passing again

--- db/schema.rb
+++ db/schema.rb
@@ -116,13 +116,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_144602) do
     t.string "name"
     t.string "category"
     t.float "price"
-    t.string "image"
-    t.integer "quantity"
-    t.integer "stock"
-    t.integer "rating"
-    t.text "description"
-    t.string "dosage"
-    t.string "effects"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
