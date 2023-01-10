33a709c2800f37228d898ca418500b5d27b0bfd3
Enock <enock.mokua@student.moringaschool.com>
2023-01-10 09:20:39 +0300

    add password validation fileds

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,20 @@ class User < ApplicationRecord
               message: "Please provide a username"
             },
             uniqueness: {
-              message: "A user with that username akready exists"
+              message: "A user with that username already exists"
             }
-  validates :password, presence: true
+  validates :password,
+            presence: true,
+            length: {
+              minimum: 8,
+              maximum: 16
+            },
+            confirmation: true,
+            format: {
+              with: /\A(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}\z/,
+              message:
+                "must include at least one lowercase letter, one uppercase letter, one digit and have minimum 8 characters"
+            }
   validates :email,
             presence: {
               message: "Please add an Email"
