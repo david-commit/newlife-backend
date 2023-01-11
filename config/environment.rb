4151a86baa30ef7ae24a2a80aeabd42f33e2a81f
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 08:05:32 +0300

    configure actionmailer in Base environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  domain: "https://newlife-production.up.railway.app",
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  user_name: "apikey",
+  password: ENV["SENDGRID_API_KEY"],
+  enable_starttls_auto: true
+}
