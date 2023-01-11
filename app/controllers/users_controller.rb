6cdc2d86f95dc4528bd12b9f1064bee2b46e5f56
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 06:02:00 +0300

    put response for the send email method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,6 +59,10 @@ class UsersController < ApplicationController
     sg = SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
 
     response = sg.client.mail._("send").post(request_body: mail.to_json)
+
+    # puts response.status_code
+    # puts response.body
+    # puts response.headers
   end
 
   def user_params
