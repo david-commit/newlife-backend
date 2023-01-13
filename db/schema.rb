7762024e9a79a293cb91a43a1c53cb34f1c33160
Enock <enock.mokua@student.moringaschool.com>
2023-01-13 22:43:19 +0300

    update schema with pg_saerch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_235755) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_194143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -122,6 +122,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_235755) do
     t.datetime "updated_at", null: false
     t.bigint "order_id", null: false
     t.index ["order_id"], name: "index_payments_on_order_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "practitioner_profiles", force: :cascade do |t|
