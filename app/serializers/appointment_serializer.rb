1b7943ee6abfc3bf3b847ae16c4295b45a7ea59b
Vincent Makokha <makokhavomondi@gmail.com>
2023-01-13 16:37:51 +0300

    Include messages to "POST /appointments" response

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -2,4 +2,5 @@ class AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :date, :approved, :appointment_type, :appointment_info, :time
   belongs_to :user
   belongs_to :practitioner
+  has_many :messages
 end
