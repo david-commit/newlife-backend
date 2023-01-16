569ecc7caed090afa765795c94313a88a5171a71
Enock <enock.mokua@student.moringaschool.com>
2023-01-16 09:57:28 +0300

    update user to include jwts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
+         :jwt_authenticatable,
          :registerable,
-         :recoverable,
-         :rememberable,
-         :validatable
+         jwt_revocation_strategy: JwtDenylist
 end
