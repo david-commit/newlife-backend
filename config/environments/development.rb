e80c34224ee541e99c3ff471eb110786f7423ef4
david-commit <david.ondiege@students.moringaschool.com>
2023-01-19 01:37:02 +0300

    Changed deployment host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "newlife-production.up.railway.app"
+  # config.hosts << "newlife-production.up.railway.app"
+  config.hosts << "newlife-backend-production.up.railway.app"
 
   # allow ngrok to work
   # config.hosts << /.*\.ngrok\.io/
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "newlife-production.up.railway.app"
+  # config.hosts << "newlife-production.up.railway.app"
+  config.hosts << "newlife-backend-production.up.railway.app"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
