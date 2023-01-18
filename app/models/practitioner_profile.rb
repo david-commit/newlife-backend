4f01873944d338483dc3e082aab26921be9bdde1
david-commit <david.ondiege@students.moringaschool.com>
2023-01-18 11:48:21 +0300

    Added Practitioner Profile validation

--- app/models/practitioner_profile.rb
+++ app/models/practitioner_profile.rb
@@ -1,6 +1,8 @@
 class PractitionerProfile < ApplicationRecord
   belongs_to :practitioner
 
+  validates :practitioner_id, uniqueness: true
+
   include PgSearch::Model
 
   pg_search_scope :search_by_name,
